3f9ba77f985e1939c926f431243466da570c46b3
Каленов Нурислам Темирович <n.kalenov@kazpost.kz>
2018-02-26 11:29:53 +0600

    Litle refactoring

--- src/main/java/examples/PdfGenerator.java
+++ src/main/java/examples/PdfGenerator.java
@@ -24,9 +24,7 @@ public class PdfGenerator {
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
 
             document.open();
-
-
-            createDocumentNumber(writer, "01020304", 535, 810);
+            createDocumentNumber(writer, "01020304", 520, 810);
             createHeader(writer);
             createFooterKaz(writer);
             createFooterRus(writer);
@@ -45,15 +43,15 @@ public class PdfGenerator {
         table.getDefaultCell().setBorder(0);
 
         Image logoImage = createLogoImage();
-        Phrase prescription = createPrescription("ПРЕДПИСАНИЕ/Заказное письмо с уведомлением");
-        Phrase prescriptionSender = createPrescriptionSender("от: УАП ДВД г.Астана, улица 20-40, д. 4/1");
-        Phrase barcode = createBarcode("DA010080119KZ");
-        Phrase recipient = createAdditionInfo("КӘДЕН ТАҢАТАР БЕКСҰЛТАНҰЛЫ");
-        Phrase location = createAdditionInfo("ул. КОРГАЛЖЫН, д. 11, корпус -, кв. 13");
-        Phrase region = createAdditionInfo("ЕСИЛЬСКИЙ РАЙОН");
-        Phrase cityAndIndex = createAdditionInfo("АСТАНА, 010000");
-        Phrase shpi = createAdditionInfo("ШПИ");
-        Phrase barcodeInText = createAdditionInfo("DA010080119KZ");
+        Phrase prescription = createHeaderPrescription("ПРЕДПИСАНИЕ/Заказное письмо с уведомлением");
+        Phrase prescriptionSender = createHeaderPrescriptionSender("от: УАП ДВД г.Астана, улица 20-40, д. 4/1");
+        Phrase barcode = createHeaderBarcode("DA010080119KZ");
+        Phrase recipient = createHeaderAdditionInfo("КӘДЕН ТАҢАТАР БЕКСҰЛТАНҰЛЫ");
+        Phrase location = createHeaderAdditionInfo("ул. КОРГАЛЖЫН, д. 11, корпус -, кв. 13");
+        Phrase region = createHeaderAdditionInfo("ЕСИЛЬСКИЙ РАЙОН");
+        Phrase cityAndIndex = createHeaderAdditionInfo("АСТАНА, 010000");
+        Phrase shpi = createHeaderAdditionInfo("ШПИ");
+        Phrase barcodeInText = createHeaderAdditionInfo("DA010080119KZ");
 
         table.addCell(logoImage);
         table.addCell(prescription);
@@ -70,60 +68,22 @@ public class PdfGenerator {
 
     }
 
-    private Phrase createAdditionInfo(String info) {
-        BaseFont bf = null;
-        try {
-            bf = BaseFont.createFont("C:\\Windows\\Fonts\\ARIAL.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Font font1 = new Font(bf, 8);
-
-
-        Phrase phrase = new Phrase(info, font1);
+    private Phrase createHeaderAdditionInfo(String info) {
+        Phrase phrase = new Phrase(info, headerInfoFont());
         return phrase;
     }
 
-    private Phrase createPrescription(String prescription) {
-        BaseFont bf = null;
-        try {
-            bf = BaseFont.createFont("C:\\Windows\\Fonts\\ARIAL.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Font font1 = new Font(bf, 8);
-
-
-        Phrase phrase = new Phrase(prescription, font1);
+    private Phrase createHeaderPrescription(String prescription) {
+        Phrase phrase = new Phrase(prescription, headerInfoFont());
         return phrase;
     }
 
-    private Phrase createPrescriptionSender(String sender) {
-        BaseFont bf = null;
-        try {
-            bf = BaseFont.createFont("C:\\Windows\\Fonts\\ARIAL.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Font font1 = new Font(bf, 8);
-
-
-        Phrase phrase = new Phrase("от: " + sender, font1);
-        PdfPCell cell1 = new PdfPCell(phrase);
-        cell1.setBorder(0);
+    private Phrase createHeaderPrescriptionSender(String sender) {
+        Phrase phrase = new Phrase("от: " + sender,  headerInfoFont());
         return phrase;
     }
 
-    private Phrase createBarcode(String barcode) {
+    private Phrase createHeaderBarcode(String barcode) {
         BaseFont bfSPI = null;
         try {
             bfSPI = BaseFont.createFont(this.getClass().getClassLoader().getResource("fonts/fre3of9x.ttf").toString(), BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
@@ -141,38 +101,21 @@ public class PdfGenerator {
         PdfPTable table = new PdfPTable(1);
         table.setTotalWidth(490);
 
-        BaseFont bf = null;
-        try {
-            bf = BaseFont.createFont("C:\\Windows\\Fonts\\ARIAL.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Font font1 = new Font(bf, 8);
-
-        Phrase phrase1 = new Phrase("Осы мәтін (хат, құжат) «Қазпошта» АҚ-ның WWW.POST.KZ порталы және/немесе «ГЭП» ақпараттық жүйесі арқылы алынған\n" +
-                "және басып шығарылған және жіберушінің электрондық құжаттының қағаз көшірмесі болып табылады.", font1);
-        PdfPCell cell1 = new PdfPCell(phrase1);
+        Phrase commonInfo = createFooterCommonInfo("Осы мәтін (хат, құжат) «Қазпошта» АҚ-ның WWW.POST.KZ порталы және/немесе «ГЭП» ақпараттық жүйесі арқылы алынған\n" +
+                "және басып шығарылған және жіберушінің электрондық құжаттының қағаз көшірмесі болып табылады.");
+        PdfPCell cell1 = new PdfPCell(commonInfo);
         cell1.setBorder(0);
         cell1.setPadding(0);
-        cell1.setBorder(PdfPCell.BOX);
 
-        Phrase phrase2 = new Phrase();
-        phrase2.add("Электрондық құжатты жасаған: УАП ДВД г.Астана");
-        PdfPCell cell2 = new PdfPCell(phrase2);
+        Phrase documentCreator = createFooterCommonInfo("Электрондық құжатты жасаған: УАП ДВД г.Астана");
+        PdfPCell cell2 = new PdfPCell(documentCreator);
         cell2.setBorder(0);
         cell2.setPadding(0);
-        cell2.setBorder(PdfPCell.BOX);
 
-        Phrase phrase3 = new Phrase();
-        phrase3.add("Хатты өңдеу күні мен уақыты: 23-02-2018 сағат 09:00");
-        PdfPCell cell3 = new PdfPCell(phrase3);
+        Phrase dateAndTime = createFooterCommonInfo("Хатты өңдеу күні мен уақыты: 19-02-2018 сағат 09:00");
+        PdfPCell cell3 = new PdfPCell(dateAndTime);
         cell3.setBorder(0);
         cell3.setPadding(0);
-        cell3.setBorder(PdfPCell.BOX);
-
-
         table.addCell(cell1);
         table.addCell(cell2);
         table.addCell(cell3);
@@ -184,42 +127,21 @@ public class PdfGenerator {
     private void createFooterRus(PdfWriter writer) {
         PdfPTable table = new PdfPTable(1);
         table.setTotalWidth(490);
-
-        BaseFont bf = null;
-        try {
-            bf = BaseFont.createFont("C:\\Windows\\Fonts\\ARIAL.TTF", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        } catch (DocumentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Font font1 = new Font(bf, 8);
-
-        Phrase phrase1 = new Phrase("Настоящий текст (письмо, документ) получен и распечатан с использованием портала WWW.POST.KZ и/или" +
-                "информационной системой «ГЭП» АО «Казпочта» и является бумажной копией электронного документа отправителя.", font1);
-        PdfPCell cell1 = new PdfPCell(phrase1);
+        Phrase commonInfo = createFooterCommonInfo("Настоящий текст (письмо, документ) получен и распечатан с использованием портала WWW.POST.KZ и/или" +
+                "информационной системой «ГЭП» АО «Казпочта» и является бумажной копией электронного документа отправителя.");
+        PdfPCell cell1 = new PdfPCell(commonInfo);
         cell1.setBorder(0);
         cell1.setPadding(0);
-        cell1.setBorder(PdfPCell.BOX);
 
-        Phrase phrase2 = new Phrase();
-        phrase2.setFont(font1);
-        phrase2.add("Электронный документ создал: УАП ДВД г.Астана");
-        PdfPCell cell2 = new PdfPCell(phrase2);
+        Phrase documentCreator = createFooterCommonInfo("Электронный документ создал: УАП ДВД г.Астана");
+        PdfPCell cell2 = new PdfPCell(documentCreator);
         cell2.setBorder(0);
         cell2.setPadding(0);
-        cell2.setBorder(PdfPCell.BOX);
 
-        Phrase phrase3 = new Phrase();
-        phrase3.setFont(font1);
-        phrase3.add("Дата и время обработки письма: 23-02-2018 в 09:00 часов");
-        phrase3.setFont(font1);
-        PdfPCell cell3 = new PdfPCell(phrase3);
+        Phrase dateAndTime = createFooterCommonInfo("Дата и время обработки письма: 23-02-2018 в 09:00 часов");
+        PdfPCell cell3 = new PdfPCell(dateAndTime);
         cell3.setBorder(0);
         cell3.setPadding(0);
-        cell3.setBorder(PdfPCell.BOX);
-
-
         table.addCell(cell1);
         table.addCell(cell2);
         table.addCell(cell3);
@@ -250,7 +172,7 @@ public class PdfGenerator {
             cb.saveState();
             cb.beginText();
             cb.moveText(x, y);
-            cb.setFontAndSize(bf, 9);
+            cb.setFontAndSize(bf, 12);
             cb.showText(text);
             cb.endText();
             cb.restoreState();
@@ -265,7 +187,7 @@ public class PdfGenerator {
         Image image = null;
         try {
             image = Image.getInstance(this.getClass().getClassLoader().getResource("images/logo_hybrid.png"));
-            image.scaleToFit(100, 70);
+            image.scaleToFit(90, 80);
 
 
         } catch (BadElementException e) {
@@ -277,5 +199,36 @@ public class PdfGenerator {
         return image;
     }
 
+    private Font headerInfoFont(){
+        Font font = fontLoader(this.getClass().getClassLoader().getResource("fonts/OpenSans-Bold.ttf").toString());
+        font.setSize(10);
+        return font;
+    }
+
+    private Font footerFont(){
+        Font font = fontLoader(this.getClass().getClassLoader().getResource("fonts/OpenSans-Bold.ttf").toString());
+        font.setSize(8);
+        return font;
+    }
+
+    private Phrase createFooterCommonInfo(String info) {
+        Phrase phrase = new Phrase(info, footerFont());
+        return phrase;
+    }
+
+    private Font fontLoader(String fontPath) {
+
+        BaseFont bf = null;
+        try {
+            bf = BaseFont.createFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Font font = new Font(bf);
+
+        return font;
+    }
 
 }
